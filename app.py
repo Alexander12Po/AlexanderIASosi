e0c03cc1923915090024696a602021165a7d270f
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-06-29 11:28:36 -0500

    Primer commit limpio

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+API_KEY = "GEMINI_API_KEY"
+
+MODEL = "gemini-2.5-flash"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    mensaje = request.json.get("mensaje")
+
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/{MODEL}:generateContent?key={API_KEY}"
+
+    data = {
+        "contents": [
+            {
+                "parts": [
+                    {"text": mensaje}
+                ]
+            }
+        ]
+    }
+
+    r = requests.post(url, json=data)
+
+    if r.status_code == 200:
+        respuesta = r.json()["candidates"][0]["content"]["parts"][0]["text"]
+        return jsonify({"respuesta": respuesta})
+
+    return jsonify({"respuesta": "Error al conectar con Gemini."})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
